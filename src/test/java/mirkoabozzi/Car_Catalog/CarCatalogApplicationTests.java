90853bdfae2610392fa85c35f50c18386a3a2326
Mirko Abozzi <mirko.abozzi@gmail.com>
2025-02-12 08:36:22 +0100

    Initial commit

---
+++ src/test/java/mirkoabozzi/Car_Catalog/CarCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package mirkoabozzi.Car_Catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
